2b0e8681d33bdea610623a4fb02850d3d00bc35e
Rick Calder <rick@calder.io>
2018-11-11 21:39:23 -0500

    Create teams endpoint

--- index.js
+++ index.js
@@ -25,8 +25,28 @@ const typeDefs = gql`
     teamId: Int
   }
 
+   type Teams {
+    id: Int 
+    name: String
+    abbreviation: String
+    teamName: String
+    locationName: String
+    firstYearOfPlay: String
+    divisionName: String
+    divisionShort: String
+    divisionId: Int
+    conferenceName: String
+    conferenceId: Int
+    franchiseId: Int
+    venueName: String
+    venueCity: String
+    venueTimeZone: String
+    officialSiteUrl: String
+  }
+
   type Query {
-    getPlayer(id: Int): Player
+    getPlayer(id: Int): Player,
+    getTeams: [Teams]
   }
 `;
 
@@ -39,6 +59,36 @@ const resolvers = {
       return root.currentTeam.id
     }
   },
+  Teams: {
+    divisionName: root => {
+      return root.division.name
+    },
+    divisionShort: root => {
+      return root.division.nameShort
+    },
+    divisionId: root => {
+      return root.division.id
+    },
+    conferenceName: root => {
+      return root.conference.name
+    },
+    conferenceId: root => {
+      return root.conference.id
+    },
+    franchiseId: root => {
+      return root.franchise.id
+    },
+    venueName: root => {
+      return root.venue.name
+    },
+    venueCity: root => {
+      return root.venue.city
+    },
+    venueTimeZone: root => {
+      return root.venue.timeZone.tz
+    },
+
+  },
   Query: {
     getPlayer: async (_, { id }) => {
       const player = await axios.get(`https://statsapi.web.nhl.com/api/v1/people/${id}`)
@@ -46,6 +96,14 @@ const resolvers = {
         return response.data.people[0]
       })
       return player
+    },    
+    getTeams: async() => {
+      const teams = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams`)
+      .then(response => {
+        console.log(response.data)
+        return response.data.teams
+      })
+      return teams
     },
   }
 }
