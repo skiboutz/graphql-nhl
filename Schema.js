d0bda6899dcc4c46eaf8d1d15faba7f0b98b9ae7
Rick Calder <rick@calder.io>
2018-11-13 09:54:57 -0500

    refactor Schema into its own file

---
+++ Schema.js
@@ -0,0 +1,28 @@
+
+const Player = require('./TypeDefs/Player')
+const Venue = require('./TypeDefs/Venue')
+const TimeZone = require('./TypeDefs/TimeZone')
+const Team = require('./TypeDefs/Team')
+const Division = require('./TypeDefs/Division')
+const Conference = require('./TypeDefs/Conference')
+const Resolvers = require('./Resolvers/Resolvers')
+const { makeExecutableSchema } = require('graphql-tools')
+
+const Query = `
+  type Query {
+    getPlayer(id: Int): Player,
+		getTeams: [Team],
+		getTeam(id: Int): Team,
+		getDivisions: [Division],
+		getDivision(id: Int): Division,
+		getConferences: [Conference],
+		getConference(id: Int): Conference
+  }
+`
+
+const schema = makeExecutableSchema({
+  typeDefs: [Query, Player, Team, Division, Conference, Venue, TimeZone],
+  resolvers: Resolvers,
+})
+
+module.exports = schema
--- index.js
+++ index.js
@@ -3,31 +3,7 @@ const PlayerAPI = require('./DataSources/PlayerAPI')
 const ConferenceAPI = require('./DataSources/ConferenceAPI')
 const DivisionAPI = require('./DataSources/DivisionAPI')
 const TeamAPI = require('./DataSources/TeamAPI')
-const Player = require('./TypeDefs/Player')
-const Venue = require('./TypeDefs/Venue')
-const TimeZone = require('./TypeDefs/TimeZone')
-const Team = require('./TypeDefs/Team')
-const Division = require('./TypeDefs/Division')
-const Conference = require('./TypeDefs/Conference')
-const Resolvers = require('./Resolvers/Resolvers')
-const { makeExecutableSchema } = require('graphql-tools')
-
-const Query = `
-  type Query {
-    getPlayer(id: Int): Player,
-		getTeams: [Team],
-		getTeam(id: Int): Team,
-		getDivisions: [Division],
-		getDivision(id: Int): Division,
-		getConferences: [Conference],
-		getConference(id: Int): Conference
-  }
-`
-
-const schema = makeExecutableSchema({
-  typeDefs: [Query, Player, Team, Division, Conference, Venue, TimeZone],
-  resolvers: Resolvers,
-})
+const schema = require('./Schema')
 
 const server = new ApolloServer({
   schema,
